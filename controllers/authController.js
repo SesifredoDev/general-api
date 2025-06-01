981bc6a72a879d9834029b708cb194b595c50723
Paolo Chaffey <93771023+SesifredoDev@users.noreply.github.com>
2025-06-01 01:25:25 +0100

    make it better

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,20 +3,59 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 
 exports.register = async (req, res) => {
-  const { username, name, email, password, avatar, icon, stats } = req.body;
+  const { username, name, email, password } = req.body;
 
   const existing = await User.findOne({ email });
   if (existing) return res.status(400).json({ message: 'User already exists' });
 
   const hashedPassword = await bcrypt.hash(password, 10);
 
+  let stats =  {
+    str:{
+      slug: "str",
+      name: "Strength",
+      level: 1,
+      xp: 0,
+    },
+    int:{
+      slug: "int",
+      name: "Intelligence",
+      level: 1,
+      xp: 0,
+    },
+    wis:{
+      slug: "wis",
+      name: "Wisdom",
+      level: 1,
+      xp: 0,
+    },
+    dex:{
+      slug: "dex",
+      name: "Dexterity",
+      level: 1,
+      xp: 0,
+    },
+    cha:{
+      slug: "cha",
+      name: "Charisma",
+      level: 1,
+      xp: 0,
+    },
+    con:{
+      slug: "con",
+      name: "Constitution",
+      level: 1,
+      xp: 0,
+    }
+  }
+
   const newUser = new User({
     username,
     name,
     email,
     password: hashedPassword,
-    avatar,
-    icon,
+    avatar: "",
+    icon:  "",
     stats
   });
 
