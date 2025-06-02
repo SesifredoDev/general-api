ad833126d665e0f5c886ed1e3f8eb782bbf3556b
Paolo Chaffey <93771023+SesifredoDev@users.noreply.github.com>
2025-06-02 17:03:31 +0100

    new entry v1 fix

--- routes/you2.js
+++ routes/you2.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const verifyToken = require('../middleware/authMiddleware');
+const you2Controller  =  require('../controllers/you2Controller');
 
-router.post('/entry')
+router.post('/entry', verifyToken,  you2Controller.newEntry)
