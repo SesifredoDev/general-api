73645065bbc88f8b3726cad66e56d3f710ff07dd
Paolo Chaffey <93771023+SesifredoDev@users.noreply.github.com>
2025-06-03 10:17:05 +0100

    friends system

--- models/userModel.js
+++ models/userModel.js
@@ -27,7 +27,9 @@ const userSchema = new mongoose.Schema({
     con: statBlockSchema,
   },
   lastEntry:String,
-  refreshTokens: [String]
+  refreshTokens: [String],
+  friends: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  friendRequests: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }]
 });
 
 
--- routes/youTwo.js
+++ routes/youTwo.js
@@ -8,4 +8,8 @@ router.get('/train', youTwoController.train);
 router.post('/entry', verifyToken,  youTwoController.newEntry);
 router.post('/avatar', verifyToken, avatarController.updateAvatar );
 
+router.post('/friend-request',verifyToken, youTwoController.sendFriendRequest);
+router.post('/accept-friend', verifyToken,youTwoController.acceptFriendRequest);
+router.get('/:userId/friends',verifyToken, youTwoController.getFriends);
+
 module.exports  =  router;
