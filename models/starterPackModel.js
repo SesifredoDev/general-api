674ee365f06233fa0f9aa631bc36843491d56ab9
Paolo Chaffey <93771023+SesifredoDev@users.noreply.github.com>
2025-06-07 14:50:50 +0100

    pack schema

--- models/starterPackModel.js
+++ models/starterPackModel.js
@@ -6,9 +6,9 @@ const {SpellSchema} =  require('./spellModel')
 const StarterPackSchema = new mongoose.Schema({
   name: { type: String, required: true },
   description: { type: String },
-  items: { type: [ItemSchema], default: [] },
-  weapons: { type: [WeaponSchema], default: [] },
-  spells: { type: [SpellSchema], default: [] }
+  items: [ItemSchema],
+  weapons: [WeaponSchema], 
+  spells: [SpellSchema] 
 });
 
 
